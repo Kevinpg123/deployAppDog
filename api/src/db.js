abb5d4f485b3d46563e315f8fd92edb8992e1800
Kevinpg123 <osmarkevinp@gmail.com>
2023-11-05 23:46:18 -0300

    preparando el back

--- api/src/db.js
+++ api/src/db.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
 } = process.env;
 // const DogsModel = require('./models/Dog');
 // const TemperamentsModel = require('./models/Temperaments');
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
